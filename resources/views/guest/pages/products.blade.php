24550d76efe87bafc578a2d67459df46500eec79
HenrySantoso <119570803+HenrySantoso@users.noreply.github.com>
2025-05-02 21:57:40 +0700

    update products

--- resources/views/guest/pages/products.blade.php
+++ resources/views/guest/pages/products.blade.php
@@ -23,41 +23,49 @@
             <div class="row">
                 <div class="col-lg-12">
                     <div class="section-heading">
-                        <h2>Our Latest Products</h2>
-                        <span>Check out all of our products.</span>
+                        <h2>Produk Terbaru Kami!</h2>
+                        <span>Temukan produk yang kamu suka!</span>
                     </div>
                 </div>
             </div>
         </div>
         <div class="container">
             <div class="row">
-                @foreach ($produks as $produk)
-                    <div class="col-lg-4">
-                        <div class="item">
-                            <div class="thumb">
-                                <div class="hover-content">
-                                    <ul>
-                                        <li><a href="{{ route('guest-singleProduct', $produk->slug) }}"><i class="fa fa-eye"></i></a>
-                                        </li>
-                                        <li><a href=""><i class="fa fa-star"></i></a></li>
-                                        <li><a href=""><i class="fa fa-shopping-cart"></i></a></li>
-                                    </ul>
+                @if ($produks->count() > 0)
+                    @foreach ($produks as $produk)
+                        <div class="col-lg-4">
+                            <div class="item">
+                                <div class="thumb">
+                                    <div class="hover-content">
+                                        <ul>
+                                            <li><a href="{{ route('guest-singleProduct', $produk->slug) }}"><i
+                                                        class="fa fa-eye"></i></a>
+                                            </li>
+                                            <li><a href=""><i class="fa fa-star"></i></a></li>
+                                            <li><a href=""><i class="fa fa-shopping-cart"></i></a></li>
+                                        </ul>
+                                    </div>
+                                    <img src="{{ asset('storage/' . $produk->fotoProduk->first()->file_foto_produk) }}"
+                                        alt="{{ $produk->nama_produk }}">
                                 </div>
-                                <img src="{{ asset('storage/' .  $produk->fotoProduk->first()->file_foto_produk) }}" alt="{{ $produk->nama_produk }}">
-                            </div>
-                            <div class="down-content">
-                                <h4>{{ $produk->nama_produk }}</h4>
-                                <span>Rp {{ number_format($produk->harga, 0, ',', '.') }}</span>
-                                <ul class="stars">
-                                    @for ($i = 0; $i < 5; $i++)
-                                        <li><i class="fa fa-star"></i></li>
-                                    @endfor
-                                </ul>
-                                <p>{{ $produk->deskripsi }}</p>
+                                <div class="down-content">
+                                    <h4>{{ $produk->nama_produk }}</h4>
+                                    <span>Rp {{ number_format($produk->harga, 0, ',', '.') }}</span>
+                                    <ul class="stars">
+                                        @for ($i = 0; $i < 5; $i++)
+                                            <li><i class="fa fa-star"></i></li>
+                                        @endfor
+                                    </ul>
+                                    <p>{{ $produk->deskripsi }}</p>
+                                </div>
                             </div>
                         </div>
+                    @endforeach
+                @else
+                    <div class="col-12 text-center">
+                        <h5>Produk belum tersedia saat ini.</h5>
                     </div>
-                @endforeach
+                @endif
             </div>
         </div>
     </section>
